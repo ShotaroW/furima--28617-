7e5410f0c0e3c7f74402a428970c30f22d63d435
ShotaroW <aufschwung.x1106sy@gmail.com>
2020-12-29 12:56:33 +0900

    Active Storageの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birth_date])
   end
- 
+
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id) 
+
 end
