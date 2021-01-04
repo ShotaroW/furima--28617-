b48bd804b304d76a361566a55df2aed567991768
ShotaroW <aufschwung.x1106sy@gmail.com>
2021-01-04 10:23:45 +0900

    テストコード2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,29 +11,59 @@ RSpec.describe Item, type: :model do
         expect(@user).to be_valid
       end
       it "商品の価格が¥300~¥9,999,999の場合に保存される"
-      @item.price [3-9][0-9]{2}|[1-9][0-9]{3,6}
-      expect(@user).to be_valid
+      @item.price ＝ "5000"
+      expect(@item).to be_valid
     end
     context "商品情報が保存できない場合" do
       it "商品名がないとは保存できない" do
+        @item.name = nil
+        @item.valid?
+        expect(@name.errors.full_messages).to include("name is invalid")
       end     
       it "商品の価格がないと保存できない" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("item is invalid")
       end
       it "商品の価格が¥300~¥9,999,999以外の場合に保存できない" do
+        @item.price = "200"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("price is invalid")
       end
       it "商品の説明がない場合に保存できない" do
+        @item.item_description = nil
+        @item.valid?
+        expect(@price.errors.full_messages).to include("item_description is invalid")
       end
       it "商品画像がない場合に保存できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("image is invalid")
       end
       it "カテゴリーの情報がない場合に保存できない" do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("category is invalid")
       end
       it "商品状態の情報がない場合に保存できない" do
+        @item.status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("status is invalid")
       end
       it "配送料の負担がない場合に保存できない" do
+        @item.fee_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("status_id is invalid")
       end
       it "発送元の情報がない場合に保存できない" do
+        @item.area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("area_id is invalid")
       end
       it "発送までの日数がない場合に保存できない" do
+        @item.area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("area_id is invalid")
       end
     end
   end
