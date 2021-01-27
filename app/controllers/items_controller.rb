97d351e23aae9d476109606de4efc38d84774860
ShotaroW <aufschwung.x1106sy@gmail.com>
2021-01-27 21:34:32 +0900

    edit,updateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,15 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
   def create
     @item = Item.new(item_params)
     if @item.save
