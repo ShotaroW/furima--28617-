5fd34f1112cc06cd85b3daf6565f30a3eb29a2b4
ShotaroW <aufschwung.x1106sy@gmail.com>
2021-02-13 23:02:39 +0900

    コントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,27 @@
 class OrdersController < ApplicationController
 
   def index
-    @order = Order.new
+    @order = Form.new
     @item = Item.find(params[:item_id])
   end
   def create
-    binding.pry
+    @order = Form.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def order_params
+    params.require(:form).permit(
+      :post_code,
+      :area_id,
+      :city,
+      :address,
+      :house_number,
+      :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+    end
 end
