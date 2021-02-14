a2a97fbf567ef55633c85e3d563e20af34b67572
ShotaroW <aufschwung.x1106sy@gmail.com>
2021-02-14 15:03:53 +0900

    テストコードの実装

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
-describe Item, type: :model do
+describe Form, type: :model do
   before do
-    @item = FactoryBot.build(:item)
+    @order = FactoryBot.build(:form)
   end
   describe '商品購入' do
 context "商品が購入できる場合" do
@@ -13,6 +13,7 @@ end
   @order.valid?
   expect(@order).to be_valid       
 end
+end
 
 context "商品が購入できない場合" do
   it "post_codeが空では登録できない" do
@@ -23,12 +24,12 @@ context "商品が購入できない場合" do
   it'post_codeにハイフンがない場合登録できない'do
   @order.post_code = 1234567
   @order.valid?
-  expect(@order.errors.full_messages).to include("Post code input correcrtly")
+  expect(@order.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
  end
- it "status_idのidが1の場合に保存できない" do
-  @order.status_id = 1
+ it "area_idのidが1の場合に保存できない" do
+  @order.area_id = 1
   @order.valid?
-  expect(@order.errors.full_messages).to include("Status must be other than 1")
+  expect(@order.errors.full_messages).to include("Area must be other than 1")
 end
 it "cityが空では登録できない" do
   @order.city = nil
@@ -36,7 +37,7 @@ it "cityが空では登録できない" do
   expect(@order.errors.full_messages).to include("City can't be blank")
 end
 it "addressが空では登録できない" do
-  @order.adress = nil
+  @order.address = nil
   @order.valid?
   expect(@order.errors.full_messages).to include("Address can't be blank")
 end
@@ -51,15 +52,21 @@ it "house_numberが空では登録できない" do
   expect(@order.errors.full_messages).to include("House number can't be blank")
 end
 it'phone_numberにハイフンがあると登録できない'do
-@order.phone_number= 080-1234-5678
+@order.phone_number= "080-1234-5678"
 @order.valid?
-expect(@order.errors.full_messages).to include("Phone number is invalid")
+expect(@order.errors.full_messages).to include("Phone number is invalid.")
 end
 it'phone_numberが11桁以上だと登録できない'do
 @order.phone_number= "090123456789"
         @order.valid?
-        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+        expect(@order.errors.full_messages).to include("Phone number is invalid.")
 end
-
-  end
+it "tokenが空では登録できないこと" do
+  @order.token = nil
+  @order.valid?
+  expect(@order.errors.full_messages).to include("Token can't be blank")
+end
+end
+end
+end
 
